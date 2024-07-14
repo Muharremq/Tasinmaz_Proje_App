60581e444ccb0acbca3ebbd391d14db774052eba
ozanmuharremsahin <ozanmuharrem9@gmail.com>
2024-07-14 23:00:40 +0300

    map eklendi

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
 
 @Component({
   selector: 'app-map',
@@ -10,6 +16,18 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
   }
 
 }

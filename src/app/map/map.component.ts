089cc427d15a8cf392689025a46bf067dff76f14
ozanmuharremsahin <ozanmuharrem9@gmail.com>
2024-07-22 14:54:51 +0300

    haritaya google map ve osm map eklendi. reset butonu eklendi. Opacity eklendi

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, AfterViewInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
@@ -8,6 +8,12 @@ import { Point } from 'ol/geom';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
+import ScaleLine from 'ol/control/ScaleLine';
+import XYZ from 'ol/source/XYZ';
+import { defaults as defaultControls } from 'ol/control';
+
+
+
 
 @Component({
   selector: 'app-map',
@@ -17,11 +23,25 @@ import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
 export class MapComponent implements AfterViewInit, OnChanges {
   @Input() mapId: string = 'map-container';
   @Input() coordinates: { lon: number, lat: number };
+  @Input() propertyLocations: { lon: number, lat: number }[] = [];
+
 
   private map: Map;
   private vectorSource: VectorSource = new VectorSource();
   private initialCoordinates = fromLonLat([35.2433, 38.9637]);
   private initialZoom = 6.3;
+
+  private osmLayer = new TileLayer({
+    source: new OSM(),
+    visible: true,
+  });
+  
+  private googleLayer = new TileLayer({
+    source: new XYZ({
+      url: 'http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}',
+    }),
+    visible: false,
+  });
 
   ngAfterViewInit(): void {
     this.initializeMap();
@@ -32,15 +52,17 @@ export class MapComponent implements AfterViewInit, OnChanges {
       const { lon, lat } = changes.coordinates.currentValue;
       this.setView(lon, lat);
     }
+    if (changes.propertyLocations && changes.propertyLocations.currentValue) {
+      this.addPropertyMarkers(changes.propertyLocations.currentValue);
+    }
   }
 
   initializeMap(): void {
     this.map = new Map({
       target: this.mapId,
       layers: [
-        new TileLayer({
-          source: new OSM()
-        }),
+        this.osmLayer,
+        this.googleLayer,
         new VectorLayer({
           source: this.vectorSource,
           style: new Style({
@@ -59,6 +81,13 @@ export class MapComponent implements AfterViewInit, OnChanges {
         center: this.initialCoordinates,
         zoom: this.initialZoom,
       }),
+      controls: defaultControls().extend([
+        new ScaleLine({
+          units: 'metric', // or 'imperial'
+          minWidth: 100,
+          target: document.getElementById('scale-line'), // Ensure this targets the correct element
+        }),
+      ]),
     });
 
     this.map.on('click', (event) => {
@@ -73,6 +102,13 @@ export class MapComponent implements AfterViewInit, OnChanges {
     });
     this.vectorSource.clear();
     this.vectorSource.addFeature(marker);
+    //get coordinates
+    const lon = coordinates[0].toFixed(6);
+    const lat = coordinates[1].toFixed(6);
+    const coordElement = document.getElementById('coordinates');
+    if (coordElement) {
+      coordElement.innerHTML = `Koordinatlar: Enlem: ${lat}, Boylam: ${lon}`;
+    }
   }
 
   setView(lon: number, lat: number, zoom: number = 10): void {
@@ -81,13 +117,39 @@ export class MapComponent implements AfterViewInit, OnChanges {
     this.addMarker([lon, lat]);
   }
 
-  displayCoordinates(event): void {
-    const coordinates = toLonLat(this.map.getEventCoordinate(event.originalEvent));
-    const lon = coordinates[0].toFixed(6);
-    const lat = coordinates[1].toFixed(6);
-    const coordElement = document.getElementById('coordinates');
-    if (coordElement) {
-      coordElement.innerHTML = `Koordinatlar: Enlem: ${lat}, Boylam: ${lon}`;
+  setViewMap(lon: number, lat: number, zoom: number = 10): void {
+    this.map.getView().setCenter(fromLonLat([lon, lat]));
+    this.map.getView().setZoom(zoom);
+  }
+
+  toggleLayer(layer: string): void {
+    if (layer === 'osm') {
+      this.osmLayer.setVisible(true);
+      this.googleLayer.setVisible(false);
+    } else {
+      this.osmLayer.setVisible(false);
+      this.googleLayer.setVisible(true);
     }
+  }
+
+  changeOpacity(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.osmLayer.setOpacity(Number(value));
+    this.googleLayer.setOpacity(Number(value));
+  }
+
+  addPropertyMarkers(locations: { lon: number, lat: number }[]): void {
+    this.vectorSource.clear();
+    locations.forEach(({ lon, lat }) => {
+      const marker = new Feature({
+        geometry: new Point(fromLonLat([lon, lat])),
+      });
+      this.vectorSource.addFeature(marker);
+    });
+  }
+
+  //reset map method
+  resetMap(): void {
+    this.setViewMap(35.2433, 38.9637, 6.3);
   }
 }

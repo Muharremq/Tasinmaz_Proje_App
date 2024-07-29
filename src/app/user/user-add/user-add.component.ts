72056e9bec34fccfecc5506769ccd1e25028ff16
ozanmuharremsahin <ozanmuharrem9@gmail.com>
2024-07-29 13:52:03 +0300

    user ekle formu kapanmıyor ama çalışıyor

--- src/app/user/user-add/user-add.component.ts
+++ src/app/user/user-add/user-add.component.ts
@@ -1,18 +1,25 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
-import { AuthService } from 'src/app/services/auth.service';
-import * as bootstrap from 'bootstrap';
-import { Router } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
+import { AuthService } from "src/app/services/auth.service";
+import * as bootstrap from "bootstrap";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-user-add',
-  templateUrl: './user-add.component.html',
-  styleUrls: ['./user-add.component.css']
+  selector: "app-user-add",
+  templateUrl: "./user-add.component.html",
+  styleUrls: ["./user-add.component.css"],
 })
 export class UserAddComponent implements OnInit {
   @Output() userAdded = new EventEmitter<void>();
-  @ViewChild('addUserModal') addUserModal: ElementRef;
+  @ViewChild("addUserModal") addUserModal: ElementRef;
 
   addUserForm: FormGroup;
 
@@ -21,20 +28,31 @@ export class UserAddComponent implements OnInit {
     private userService: UserService,
     private authService: AuthService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.createAddUserForm();
   }
 
+  ngAfterViewInit() {
+    const modalElement = this.addUserModal.nativeElement;
+    modalElement.addEventListener("hidden.bs.modal", () => {
+      this.resetForm();
+      const modalBackdrop = document.querySelector(".modal-backdrop");
+      if (modalBackdrop) {
+        modalBackdrop.remove();
+      }
+    });
+  }
+
   createAddUserForm() {
     this.addUserForm = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, this.passwordValidator]],
-      phone: ['', Validators.required],
-      role: ['', Validators.required],
+      name: ["", Validators.required],
+      surname: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, this.passwordValidator]],
+      phone: ["", Validators.required],
+      role: ["", Validators.required],
     });
   }
 
@@ -43,13 +61,13 @@ export class UserAddComponent implements OnInit {
     if (!password) {
       return null;
     }
-    const haslength = password.length >= 8;
+    const hasLength = password.length >= 8;
     const hasNumber = /[0-9]/.test(password);
     const hasUpper = /[A-Z]/.test(password);
     const hasLower = /[a-z]/.test(password);
     const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
-    const valid = hasNumber && hasUpper && hasLower && hasSpecial && haslength;
+    const valid = hasNumber && hasUpper && hasLower && hasSpecial && hasLength;
     if (!valid) {
       return { passwordInvalid: true };
     }
@@ -62,21 +80,29 @@ export class UserAddComponent implements OnInit {
       this.authService.register(registerUser).subscribe(
         () => {
           this.userAdded.emit();
-          const modalElement = this.addUserModal.nativeElement;
-          const modalInstance = bootstrap.Modal.getInstance(modalElement);
-          modalInstance.hide();
-          this.resetForm();
-          this.router.navigateByUrl('/user');
+          this.closeModal();
+          this.router.navigate(["/user"]).then(() => {
+            console.log("Navigated to user page");
+          });
         },
-        error => {
+        (error) => {
           console.error("User registration error: ", error);
         }
       );
     }
   }
 
-  //reset form
+  closeModal(): void {
+    const modalElement = this.addUserModal.nativeElement;
+    const modalInstance = new bootstrap.Modal(modalElement); // Modal instance alıyoruz
+    modalInstance.hide(); // Modalı kapatıyoruz
+    this.resetForm(); // Formu sıfırlıyoruz
+  }
+
   resetForm() {
     this.addUserForm.reset();
+    this.addUserForm.markAsPristine();
+    this.addUserForm.markAsUntouched();
+    this.addUserForm.updateValueAndValidity();
   }
 }
